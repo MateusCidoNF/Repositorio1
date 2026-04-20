f293d69df348b4a15669eaf60ad4c5fcf75e641a
MateusCidoNF <mat.suetam@gmail.com>
2026-04-20 01:34:10 -0300

    Update cronometro.py
    Adiciona formatação de MM:SS

--- cronometro.py
+++ cronometro.py
@@ -5,3 +5,7 @@ print("Cronômetro Iniciado!")
 
 def limpar_tela():
   os.system('cls' if os.name == 'nt' else 'clear')
+
+def formatar_tempo(segundos):
+  minutos, segs = divmod(segundos, 60)
+  return f"{minutos:02d}:{segs:02d}"
