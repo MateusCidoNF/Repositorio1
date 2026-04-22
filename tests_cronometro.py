793268fb5a74d927c54aa7171b468bb715f07ea4
MateusCidoNF <mat.suetam@gmail.com>
2026-04-22 15:10:22 -0300

    Update tests_cronometro.py
    Ajustes de texto

--- tests_cronometro.py
+++ tests_cronometro.py
@@ -2,17 +2,17 @@
 
 import cronometro
 
-def teste_tempo_zero():
+def test_tempo_zero():
   assert cronometro.formatar_tempo(0) == "00:00"
 
-def teste_tempo_segundos():
+def test_tempo_segundos():
   assert cronometro.formatar_tempo(30) == "00:30"
 
-def teste_tempo_minuto():
+def test_tempo_minuto():
   assert cronometro.formatar_tempo(60) == "01:00"
 
-def teste_tempo_misto():
+def test_tempo_misto():
   assert cronometro.formatar_tempo(90) == "01:30"
 
-def teste_tempo_grande():
+def test_tempo_grande():
   assert cronometro.formatar_tempo(600) == "10:00"
