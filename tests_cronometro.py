6b3d5f8a93ece9df4a942b1c10a65a61612b6328
MateusCidoNF <mat.suetam@gmail.com>
2026-04-22 14:57:34 -0300

    Update tests_cronometro.py
    Mudanças de erros dentro do arquivo

--- tests_cronometro.py
+++ tests_cronometro.py
@@ -5,14 +5,14 @@ import cronometro
 def teste_tempo_zero():
   assert cronometro.formatar_tempo(0) == "00:00"
 
-def teste_tempo_segundos(0):
+def teste_tempo_segundos():
   assert cronometro.formatar_tempo(30) == "00:30"
 
-def teste_tempo_minuto(0):
+def teste_tempo_minuto():
   assert cronometro.formatar_tempo(60) == "01:00"
 
-def teste_tempo_misto(0):
+def teste_tempo_misto():
   assert cronometro.formatar_tempo(90) == "01:30"
 
-def teste_tempo_grande(0):
+def teste_tempo_grande():
   assert cronometro.formatar_tempo(600) == "10:00"
